f8b7ee8ddfa0739b4f67bfd251c180a82b3184cc
Thomas Duval <thomas.duval@opmbx.org>
2016-02-08 11:05:06 +0100

    Fix link between worktimer & breaktime

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,92 +1,83 @@
+/* global $ */
 var worktime = 2;
 var breaktime = 1;
 var workduration = worktime * 60 * 1000;
 var breakduration = breaktime * 60 * 1000;
+var timerMinute;
+var timerSecond;
+var timerintervalID;
+var timerendID;
+var transitionToBreak;
+var transitionToWork;
 
 function workTimer() {
-	// PLAY NOISE 
-	timerMinute = worktime;
-	timerSecond = 60;
-	timerMinute--;
-	$("#timerMinuteID").html(timerMinute);
-	timerintervalID = setInterval(timer, 1000);
-	timerendID = setTimeout(clearTimer, workduration);
-	transitionToBreak = setTimeout(breakTimer, workduration);
+    // PLAY NOISE 
+    timerMinute = worktime;
+    timerSecond = 60;
+    timerMinute--;
+    $("#timerMinuteID").html(timerMinute);
+    timerintervalID = setInterval(timer, 1000);
+    timerendID = setTimeout(clearTimer, workduration);
+    transitionToBreak = setTimeout(breakTimer, workduration+1);
 }
 
 function timer() {
-	if (timerSecond > 0) {
-		timerSecond--;
-		$("#timerSecondID").html(timerSecond);
-	} else {
-		timerMinute--;
-		$("#timerMinuteID").html(timerMinute);
-		timerSecond = 60;
-	}
+    if (timerSecond > 0) {
+        timerSecond--;
+        $("#timerSecondID").html(timerSecond);
+    } else {
+        timerMinute--;
+        $("#timerMinuteID").html(timerMinute);
+        timerSecond = 60;
+    }
 }
 
 function clearTimer() {
-	window.clearInterval(timerintervalID);
+    window.clearInterval(timerintervalID);
 }
 
 function breakTimer() {
-	// PLAY NOISE 
-	timerMinute = breaktime;
-	timerSecond = 60;
-	timerMinute--;
-	$("#timerMinuteID").html(timerMinute);
-	timerintervalID = setInterval(timer, 1000);
-	timerendID = setTimeout(clearTimer, breakduration);
-	transitionToWork = setTimeout(workTimer, breakduration);
+    // PLAY NOISE 
+    timerMinute = breaktime;
+    timerSecond = 60;
+    timerMinute--;
+    $("#timerMinuteID").html(timerMinute);
+    timerintervalID = setInterval(timer, 1000);
+    timerendID = setTimeout(clearTimer, breakduration);
+    transitionToWork = setTimeout(workTimer, breakduration);
 }
 
 function stop() {
-	$("#timerMinuteID").html(worktime);
-	$("#timerSecondID").html(00);
-	clearInterval(timerintervalID);
-	clearTimeout(transitionToBreak);
-	clearTimeout(transitionToWork);
-	clearTimeout(timerendID);
+    $("#timerMinuteID").html(worktime);
+    $("#timerSecondID").html(0);
+    clearInterval(timerintervalID);
+    clearTimeout(transitionToBreak);
+    clearTimeout(transitionToWork);
+    clearTimeout(timerendID);
 
 }
 
-function workTimeMore() {
-	worktime++;
-	$("#worktimenumber").html(worktime);
-}
-
-function workTimeLess() {
-	worktime--;
-	$("#worktimenumber").html(worktime);
-}
-
-function BreakTimeMore() {
-	breaktime++;
-	$("#breaktimenumber").html(breaktime);
-}
-
-function BreakTimeLess() {
-	breaktime--;
-	$("#breaktimenumber").html(breaktime);
-}
-
-$(document).ready(function() {
-	$('#workTimeMore').click(function() {
-		workTimeMore();
-	});
-	$('#workTimeLess').click(function() {
-		workTimeLess();
-	});
-	$('#BreakTimeMore').click(function() {
-		BreakTimeMore();
-	});
-	$('#BreakTimeLess').click(function() {
-		BreakTimeLess();
-	});
-	$('#start').click(function() {
-		workTimer();
-	});
-	$('#stop').click(function() {
-		stop();
-	});
+$(document).ready(function () {
+    $('#workTimeMore').click(function () {
+        worktime++;
+        $("#worktimenumber").html(worktime);
+    });
+    $('#workTimeLess').click(function () {
+        worktime--;
+        $("#worktimenumber").html(worktime);
+    });
+    $('#BreakTimeMore').click(function () {
+        breaktime++;
+        $("#breaktimenumber").html(breaktime);
+    });
+    $('#BreakTimeLess').click(function () {
+        breaktime--;
+        $("#breaktimenumber").html(breaktime);
+    });
+    $('#start').click(function () {
+        workTimer();
+    });
+    $('#stop').click(function () {
+        stop();
+    });
 });
